770e7607136ae4113f53df1d909424d9d635b7a3
Anna Potempska <anna.potempska@icloud.com>
2024-11-08 21:44:57 +0100

    finished navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,19 @@
 function Navbar() {
     return (
-        <div className="navbar pb-2 border-b border-gray-200 p-4 flex justify-between items-center">
-            <div className="logo">
-                <h1 className="text-gray-800 text-4xl font-bold uppercase ">📝 Grocery List</h1>
-            </div>
-            <ul className="flex"> 
-                <li className="text-primary">Home</li>
-                <li className="text-primary ml-2">Create new list</li>
-            </ul>
+        <div className="navbar pb-2 border-b border-gray-200 p-4 flex justify-between items-center max-sm:flex-col">
+            <div className="logo flex items-center">
+                <div className="flex items-center">
+                    <svg className="inline-block w-10 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="black" class="size-10">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                    </svg>
+                    <h1 className="text-gray-800 text-4xl font-bold uppercase "> Grocery List</h1>
+                </div>
             
+            </div>
+            <ul className="flex items-center gap-3 max-sm:mt-3">
+                <li className="text-primary ml-2 uppercase text-xs font-bold cursor-pointer">Home</li>
+                <li className="btn text-primary transition ease-out duration-500">Create new list</li>
+            </ul>    
         </div>
     );
 }
--- src/mainpage.js
+++ src/mainpage.js
@@ -6,39 +6,63 @@ function Mainpage() {
                 <div className="card">
                     <img src="/img/img_1.jpg" alt="Party items" className="w-full h-auto" />
                     <div  className="m-4">
-                        <h3 className="text-gray-800 text-xl mt-4 pb-2 border-b border-gray-200">Home</h3>
+                        <div className=" text-xl mt-4 pb-2 border-b border-gray-200 flex justify-between items-center">
+                            <h3 className="text-gray-800 ">Home</h3>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
+                            </svg>
+                        </div>
                         <div className="flex items-center">
                             <input type="checkbox" id="myCheckbox" class="checkbox"/>
                             <label for="myCheckbox" class="text-gray-700 ml-2 ">Coffee</label>
                         </div>
                     </div>
-                    <div className="badge">
+                    <div className="badge flex justify-center items-center gap-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
+                    </svg>
                         <span>Today</span>
                     </div>
                 </div>
                 <div className="card">
                     <img src="/img/img_1.jpg" alt="Party items" className="w-full h-auto" />
                     <div className="m-4">
-                        <h3 className="text-gray-800 text-xl mt-4 pb-2 border-b border-gray-200">Party</h3>
+                    <div className=" text-xl mt-4 pb-2 border-b border-gray-200 flex justify-between items-center">
+                            <h3 className="text-gray-800 ">Party</h3>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
+                            </svg>
+                        </div>
                         <div className="flex items-center">
                             <input type="checkbox" id="myCheckbox" class="checkbox"/>
                             <label for="myCheckbox" class="text-gray-700 ml-2 ">Ballons</label>
                         </div>
                     </div>
-                    <div className="badge">
+                    <div className="badge flex justify-center items-center gap-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
+                    </svg>
                         <span>For later</span>
                     </div>
                 </div>
                 <div className="card">
                     <img src="/img/img_1.jpg" alt="Party items" className="w-full h-auto" />
                     <div className="m-4">
-                        <h3 className="text-gray-800 text-xl mt-4 pb-2 border-b border-gray-200">Cake</h3>
+                        <div className=" text-xl mt-4 pb-2 border-b border-gray-200 flex justify-between items-center">
+                            <h3 className="text-gray-800 ">Cake</h3>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
+                            </svg>
+                        </div>
                         <div className="flex items-center">
                             <input type="checkbox" id="myCheckbox" class="checkbox"/>
                             <label for="myCheckbox" class="text-gray-700 ml-2 ">Eggs</label>
                         </div>
                     </div>
-                    <div className="badge">
+                    <div className="badge flex justify-center items-center gap-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
+                    </svg>
                         <span>Weekend</span>
                     </div>
                 </div>
