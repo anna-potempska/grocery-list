1b9106eea742d68176b54d441595bae401da9648
Anna Potempska <anna.potempska@icloud.com>
2024-11-08 20:44:05 +0100

    finished grocery lists

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,12 @@
 function Navbar() {
     return (
-        <div className="navbar pb-2 border-b border-gray-200">
+        <div className="navbar pb-2 border-b border-gray-200 p-4 flex justify-between items-center">
             <div className="logo">
-                <h1 className="text-gray-800 text-4xl font-bold uppercase p-4">📝 Grocery List</h1>
+                <h1 className="text-gray-800 text-4xl font-bold uppercase ">📝 Grocery List</h1>
             </div>
-            <ul>
+            <ul className="flex"> 
                 <li className="text-primary">Home</li>
-                <li className="text-primary">Create new list</li>
+                <li className="text-primary ml-2">Create new list</li>
             </ul>
             
         </div>
--- src/mainpage.js
+++ src/mainpage.js
@@ -1,15 +1,48 @@
 function Mainpage() {
     return (
-        <div className="px-16 py-6">
+        <div className="px-16 py-6 bg-gray-100">
             <h2 className="text-gray-800 text-2xl font-bold">Your Grocery Lists</h2>
-            <div className="mt-8">
-                <h3 className="text-gray-800 text-xl mt-12 pb-2 border-b border-gray-200">Home</h3>
-                <p>Coffe</p>
-            </div>
-            <div className="mt-8">
-                <h3 className="text-gray-800 text-xl mt-12 pb-2 border-b border-gray-200">Party</h3>
-                <p>Chips</p>
-            </div>
+            <div className="lg:grid grid-cols-3 gap-5">
+                <div className="card">
+                    <img src="/img/img_1.jpg" alt="Party items" className="w-full h-auto" />
+                    <div  className="m-4">
+                        <h3 className="text-gray-800 text-xl mt-4 pb-2 border-b border-gray-200">Home</h3>
+                        <div className="flex items-center">
+                            <input type="checkbox" id="myCheckbox" class="checkbox"/>
+                            <label for="myCheckbox" class="text-gray-700 ml-2 ">Coffee</label>
+                        </div>
+                    </div>
+                    <div className="badge">
+                        <span>Today</span>
+                    </div>
+                </div>
+                <div className="card">
+                    <img src="/img/img_1.jpg" alt="Party items" className="w-full h-auto" />
+                    <div className="m-4">
+                        <h3 className="text-gray-800 text-xl mt-4 pb-2 border-b border-gray-200">Party</h3>
+                        <div className="flex items-center">
+                            <input type="checkbox" id="myCheckbox" class="checkbox"/>
+                            <label for="myCheckbox" class="text-gray-700 ml-2 ">Ballons</label>
+                        </div>
+                    </div>
+                    <div className="badge">
+                        <span>For later</span>
+                    </div>
+                </div>
+                <div className="card">
+                    <img src="/img/img_1.jpg" alt="Party items" className="w-full h-auto" />
+                    <div className="m-4">
+                        <h3 className="text-gray-800 text-xl mt-4 pb-2 border-b border-gray-200">Cake</h3>
+                        <div className="flex items-center">
+                            <input type="checkbox" id="myCheckbox" class="checkbox"/>
+                            <label for="myCheckbox" class="text-gray-700 ml-2 ">Eggs</label>
+                        </div>
+                    </div>
+                    <div className="badge">
+                        <span>Weekend</span>
+                    </div>
+                </div>
+                </div>
         </div>
     );
 }
